b59115b79df716bec2d32133bd99d6845b8b30fe
browniefed <iam@browniefed.com>
2013-04-24 17:46:56 -0700

    buses are showing up, need to make them move poor logic in identifying the same bus, needs fixing
    Signed-off-by: browniefed <iam@browniefed.com>

--- firebus.js
+++ firebus.js
@@ -3,36 +3,49 @@ var map;
 
 function initialize() {
   var mapOptions = {
-    center: new google.maps.LatLng(37.7789, -122.3917),
+    center: new google.maps.LatLng(45.52355,-122.675808),
     zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 }
       
-var f = new Firebase("https://livemet.firebaseio.com/sf-muni");
+var f = new Firebase("https://livemet.firebaseio.com/port");
 
-function newBus(bus, firebaseId) {
-    var busLatLng = new google.maps.LatLng(bus.lat, bus.lon);
-    var directionColor = bus.dirTag && bus.dirTag.indexOf('OB') > -1 ? "7094FF" : "FF6262";
-    var marker = new google.maps.Marker({ icon: 'http://chart.googleapis.com/chart?chst=d_bubble_icon_text_small&chld=bus|bbT|'+bus.routeTag+'|' + directionColor + '|eee', position: busLatLng, map: map });
-    buses[firebaseId] = marker;
+function newBus(bus, firebaseId,busname) {
+    var busLatLng = new google.maps.LatLng(bus.lat, bus.lng);
+    var directionColor = "7094FF";
+    var marker = new google.maps.Marker({ icon: 'http://chart.googleapis.com/chart?chst=d_bubble_icon_text_small&chld=bus|bbT|'+firebaseId+'|' + directionColor + '|eee', position: busLatLng, map: map });
+    buses[firebaseId+bus] = marker;
 }
 
 f.once("value", function(s) {
   s.forEach(function(b) {
-    newBus(b.val(), b.name());
+
+	var name = b.name();
+	var buses = b.val();
+	for (bus in buses) 
+	{
+		newBus(buses[bus],name, bus);
+	}
   });
 });
 
 f.on("child_changed", function(s) {
-  var busMarker = buses[s.name()];
-  if(typeof busMarker === 'undefined') {
-    newBus(s.val(), s.name());
-  }
-  else {
-    busMarker.animatedMoveTo(s.val().lat, s.val().lon);
-  }
+	var route = s.name();
+	s.forEach(function(b) {
+		var name = b.name();
+		var busMarker = buses[route + name];
+
+		if (typeof busMarker === 'undefined')
+		{
+			newBus(bus.val(),route,name);
+		} 
+		else
+		{
+			busMarker.animatedMoveTo(b.val().lat,b.val().lng);
+		}
+	});
 });
 
 f.on("child_removed", function(s) {
